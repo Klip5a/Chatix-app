689707407b2274288e870a81468e4314dfccf9c1
Maksim Demin <lazerbake@gmail.com>
2022-03-30 01:13:10 +0500

    Initial commit

---
+++ src/components/ChatComponents/Chat.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import ViewContact from './ViewContact';
+
+const Chat = () => {
+    return (
+        <>
+            <Sidebar/>
+            <h1>Chat</h1>
+            <ViewContact/>
+        </>
+    );
+};
+
+export default Chat;
