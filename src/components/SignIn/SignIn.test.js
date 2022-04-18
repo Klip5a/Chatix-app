45d29f46c71cb99823fdc427d20f76f60aaf89ad
Maksim Demin <lazerbake@gmail.com>
2022-04-18 21:53:54 +0500

    add test SignIn tag Form

---
+++ src/components/SignIn/SignIn.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import SignIn from './SignIn';
+import store from '../../store/configureStore';
+
+describe('SignIn', () => {
+  it('render Form ', () => {
+    const component = mount(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SignIn />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(component.find('Form')).toHaveLength(1);
+  });
+});
